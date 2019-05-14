7caf948c37b2090f82c3cb491c1e8785d1f60f8b
a.baronskiy <a.baronskiy@mail.ru>
2019-05-14 19:25:06 +0300

    тюнинг postgres-a

--- dump.sql
+++ dump.sql
@@ -1,7 +1,15 @@
 create extension citext;
 
 SET LOCAL synchronous_commit to OFF;
--- SET LOCAL full_page_writes to OFF;
+SET LOCAL full_page_writes to OFF;
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
 
 create table users (
   id bigserial not null,
