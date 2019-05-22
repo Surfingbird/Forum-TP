dde6755f046c49126601a6113e4a915367591293
a.baronskiy <a.baronskiy@mail.ru>
2019-05-22 15:29:50 +0300

    add easyjson file

--- api/json_structs.go
+++ api/json_structs.go
@@ -82,3 +82,15 @@ type FullPost struct {
 type Error struct {
 	Message string `json:"message"`
 }
+
+type Post struct {
+	Author   string    `json:"author"`
+	Created  time.Time `json:"created"`
+	Forum    string    `json:"forum"`
+	Id       uint      `json:"id"`
+	IsEdited bool      `json:"isEdited"`
+	Message  string    `json:"message"`
+	Parent   int64     `json:"parent"`
+	Thread   uint      `json:"thread"`
+	Root     int64     `json:"-"`
+}
